be54cce508712ad3f7941205ea4d75f44b186ec7
k3llydev <hello@k3lly.dev>
2022-09-26 11:39:22 -0500

    Fix: Added missing description impl

--- src/steps/build/normalizeArchive.ts
+++ src/steps/build/normalizeArchive.ts
@@ -1,13 +1,14 @@
 export default (CONFIG: ConfigurationFile, archive: RawArchiveFile[]) => {
     const sanitizedArchive: ArchiveFile[] = [];
     for(let i = 0; i < archive.length; i++) {
-        const { title, author, tags, complexity, category, hash, date, last_modified } = archive[i];
+        const { title, author, tags, complexity, category, hash, date, last_modified, description } = archive[i];
         sanitizedArchive.push({
             title,
             author,
             date,
             last_modified,
             tags,
+            description,
             hash,
             complexity,
             category,
